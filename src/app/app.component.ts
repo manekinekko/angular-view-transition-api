74671b16c6f80939e93776bf1e2d4eba29d577a0
Wassim Chegham <github@wassim.dev>
2023-03-09 23:56:52 +0100

    chore: make it work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,99 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  NgZone,
+  ViewChild,
+} from "@angular/core";
+import { Router, RouterModule } from "@angular/router";
+import { MatCardModule } from "@angular/material/card";
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
-  `,
-  styles: []
+  selector: "app-root",
+  standalone: true,
+  imports: [RouterModule],
+  template: `<router-outlet></router-outlet>`,
 })
-export class AppComponent {
-  title = 'app';
+export class RootComponent {}
+
+@Component({
+  selector: "app-a",
+  standalone: true,
+  imports: [MatCardModule],
+  template: `
+    <mat-card #card>
+      <mat-card-content>
+        <section>A</section>
+      </mat-card-content>
+    </mat-card>
+  `,
+  styles: [
+    `
+      mat-card {
+        position: absolute;
+        width: 100px;
+        height: 100px;
+        top: 0;
+        left: 0;
+        background-color: lightblue;
+        view-transition-name: card-small;
+      }
+    `,
+  ],
+})
+export class AComponent {
+  @ViewChild("card", { static: true, read: ElementRef }) card!: ElementRef;
+  constructor(private router: Router, private zone: NgZone) {}
+
+  @HostListener("click")
+  startTransition() {
+    this.card.nativeElement.style.viewTransitionName = "card-large";
+    (document as any).startViewTransition(() => {
+      this.zone.runTask(async () => {
+        await this.router.navigateByUrl("/b");
+        this.card.nativeElement.style.viewTransitionName = "";
+      });
+    });
+  }
+}
+
+@Component({
+  selector: "app-b",
+  standalone: true,
+  imports: [MatCardModule],
+  template: `
+    <mat-card #card>
+      <mat-card-content>
+        <section>B</section>
+      </mat-card-content>
+    </mat-card>
+  `,
+  styles: [
+    `
+      mat-card {
+        width: 400px;
+        height: 400px;
+        position: absolute;
+        top: 200px;
+        left: 200px;
+        background-color: hotpink;
+        view-transition-name: card-large;
+      }
+    `,
+  ],
+})
+export class BComponent {
+  @ViewChild("card", { static: true, read: ElementRef }) card!: ElementRef;
+  constructor(private router: Router, private zone: NgZone) {}
+
+  @HostListener("click")
+  startTransition() {
+    this.card.nativeElement.style.viewTransitionName = "card-small";
+    (document as any).startViewTransition(() => {
+      this.zone.runTask(() => {
+        this.router.navigateByUrl("/a");
+        this.card.nativeElement.style.viewTransitionName = "";
+      });
+    });
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,28 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from "@angular/platform-browser";
+import { importProvidersFrom } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { provideRouter } from "@angular/router";
+import { AComponent, BComponent, RootComponent } from "./app/app.component";
 
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(RootComponent, {
+  providers: [
+    importProvidersFrom(BrowserAnimationsModule),
+    provideRouter([
+      {
+        path: "",
+        redirectTo: "a",
+        pathMatch: "full",
+      },
+      {
+        path: "a",
+        component: AComponent,
+      },
+      {
+        path: "b",
+        component: BComponent,
+      },
+    ]),
+  ],
+})
+  .then((success) => console.log(`Bootstrap success`))
+  .catch((error) => console.error(error));
